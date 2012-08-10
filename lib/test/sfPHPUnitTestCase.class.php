38e7e35d58da51dc3b5848f25a702ae9d2916024
Michael Smith <msmith@textilemanagement.com>
2012-08-09 20:39:01 -0500

    add load db method

--- lib/test/sfPHPUnitTestCase.class.php
+++ lib/test/sfPHPUnitTestCase.class.php
@@ -18,8 +18,44 @@
  */
 abstract class sfPHPUnitTestCase extends sfPHPUnitBaseTestCase
 {
+    protected static $first = true;
+
     protected function setupDB(){
         $this->db = new sfDatabaseManager(ProjectConfiguration::getApplicationConfiguration('frontend', 'test', true));
+    }
+
+    protected function loadDB($force = false){
+        if(! $this->db){
+            $this->setupDB();
+        }
+
+        $lastTime = 0;
+        if(file_exists($log_file = sfConfig::get('sf_log_dir') . '/last_phpunit_time')){
+            $lastTime = file_get_contents($log_file);
+        }
+
+        if((self::$first && time() > $lastTime + 900) || $force){
+            Doctrine_Core::loadModels(sfConfig::get('sf_lib_dir').'/model/doctrine');
+
+            Doctrine_Core::dropDatabases();
+            Doctrine_Core::createDatabases();
+            Doctrine_Core::createTablesFromModels(sfConfig::get('sf_lib_dir') . '/model');
+            Doctrine_Core::loadData(array(sfConfig::get('sf_data_dir') . '/fixtures', sfConfig::get('sf_test_dir') . '/fixtures/functional'));
+
+            SalesEntityTable::getInstance()->createQuery('s')->update()->set('use_address_validation', '?', '0')->execute();
+
+            Doctrine_Manager::getInstance()->setAttribute(Doctrine_Core::ATTR_AUTO_FREE_QUERY_OBJECTS, true);;
+        }
+
+        file_put_contents($log_file, time());
+        self::$first = false;
+
+        try{
+            Doctrine_Manager::getInstance()->getCurrentConnection()->rollback();
+        }catch (Exception $e){
+
+        }
+        Doctrine_Manager::getInstance()->getCurrentConnection()->beginTransaction();
     }
 
 }
